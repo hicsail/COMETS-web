21626fc79a2a76244987efdcdcacda9cdb1870a7
cbolles <collinbolles@gmail.com>
2024-10-26 15:03:56 -0400

    Begin creation of result type

--- packages/backend/src/simulation/models/result.model.ts
+++ packages/backend/src/simulation/models/result.model.ts
@@ -1,12 +1,48 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema()
 @ObjectType()
-export class SimulationResult {
+export class ResultOutput {
+  @Prop()
   @Field()
-  dummy: string;
+  key: string;
+
+  @Prop()
+  @Field()
+  name: string;
+
+  @Prop()
+  @Field()
+  location: string;
+}
+
+export const ResultOutputSchema = SchemaFactory.createForClass(ResultOutput);
+
+@Schema()
+@ObjectType()
+export class FluxOutput {
+  @Prop()
+  @Field()
+  modelID: string;
+
+  @Prop()
+  @Field()
+  modelName: string;
+
+  @Prop({ type: [ResultOutput]})
+}
+
+
+@Schema()
+@ObjectType()
+export class SimulationResult {
+  @Prop({ type: [ResultOutput]})
+  @Field(() => [ResultOutput])
+  biomass: ResultOutput;
+
+  @Prop({ type: [ResultOutput] })
 }
 
 export type SimulationResultDocument = SimulationResult & Document;
