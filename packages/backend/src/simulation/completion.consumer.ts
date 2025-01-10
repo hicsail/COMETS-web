8bf8b4c18de77fd27562e4841dfb8362db417de7
cbolles <collinbolles@gmail.com>
2025-01-10 16:03:18 -0500

    Add more docstrings

--- packages/backend/src/simulation/completion.consumer.ts
+++ packages/backend/src/simulation/completion.consumer.ts
@@ -4,12 +4,22 @@ import { Job } from 'bullmq';
 import { SimulationService } from './simulation.service';
 import { EmailService } from '../email/email.service';
 
+/**
+ * BullMQ processor which handles completion notifications from the COMETS-Runner.
+ * Stores the results and sends the success message to the user.
+ */
 @Processor('completion')
 export class SimulationCompletionProcessor extends WorkerHost {
   constructor(private readonly simulationService: SimulationService, private readonly emailService: EmailService) {
     super();
   }
 
+  /**
+   * Handles processing completion messages. Sends the results to the database for storage then
+   * sends the success email to the user.
+   *
+   * @param job The queue element containing the simulation results
+   */
   async process(job: Job<SimulationResult, any, string>): Promise<any> {
     const result = job.data;
     const request = await this.simulationService.makeComplete(result.requestID, result);
--- packages/backend/src/simulation/simulation.consumer.ts
+++ packages/backend/src/simulation/simulation.consumer.ts
@@ -4,12 +4,23 @@ import { EmailService } from 'src/email/email.service';
 import { JobService, JobStatus } from '../job/job.service';
 import { SimulationRequest } from './models/request.model';
 
+/**
+ * BullMQ processor which handles triggering COMETS-Runner and listening for pod success/failure.
+ * Acts as a manager to have jobs queued and executed one-by-one.
+ */
 @Processor('simulationRequest')
 export class SimulationRequestConsumer extends WorkerHost {
   constructor(private readonly jobService: JobService, private readonly emailService: EmailService) {
     super();
   }
 
+  /**
+   * Handles simulation requeust messages. Leverages the JobService to actually run the job then
+   * waits for the pod to complete. If the pod is in an error state, then a failure message is
+   * sent to the user and mainter (different messages). In either case, the pod is then deleted.
+   *
+   * @param job The queue element containing the simulation request
+   */
   async process(job: Job<SimulationRequest, any, string>): Promise<any> {
     const request = job.data;
     // Start the COMETs job
@@ -24,6 +35,13 @@ export class SimulationRequestConsumer extends WorkerHost {
     }
   }
 
+  /**
+   * Handles when the pod was found to be in an error state. The logs are taken from the pod,
+   * the failure messages are sent out, then the pod is deleted.
+   *
+   * @param request The request of the failed job
+   * @param jobName The Kubernetes job name
+   */
   private async handleError(request: SimulationRequest, jobName: string): Promise<void> {
     // Get the pod logs
     const logs = await this.jobService.getPodLogs(jobName);
@@ -35,6 +53,12 @@ export class SimulationRequestConsumer extends WorkerHost {
     await this.jobService.deleteJob(jobName);
   }
 
+  /**
+   * Handles when the pod was found to have finished successfully. The pod is simply deleted
+   *
+   * @param _request The request of the successful job
+   * @param jobName The Kubernetes job name
+   */
   private async handleSuccess(_request: SimulationRequest, jobName: string): Promise<void> {
     // Simply delete the job service, the completion consumer handles jobs that
     // gracefully completed
@@ -42,7 +66,10 @@ export class SimulationRequestConsumer extends WorkerHost {
   }
 
   /**
-   * Keep checking the status of the job once a second
+   * Keep checking the status of the job once a second. Leverages the JobService to get the status
+   * of the Job.
+   *
+   * @param jobName The Kubernetes job name
    */
   private async awaitCompletion(jobName: string): Promise<JobStatus> {
     return new Promise(async (resolve, reject) => {
